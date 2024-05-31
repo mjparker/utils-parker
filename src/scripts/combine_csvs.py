40349b4bdba1f37fe26f3e44b7fbd8988a054bb3
Parker <hey@parkernote.com>
2024-05-31 15:50:45 -0400

    setup combine-csvs click command

--- src/scripts/combine_csvs.py
+++ src/scripts/combine_csvs.py
@@ -1,49 +1,44 @@
-# pylint: disable=pointless-string-statement
-import sys
 import os
 import pandas as pd
-
-"""
-Combine multiple CSV files into one. Assumes that all CSVs have the same columns.
-Accepts a directory path as a command line argument.
-
-Usage:
-    python combine_csvs_directory.py <directory_path>
-"""
+import click
 
 
-# Check if the command line argument is provided
-if len(sys.argv) != 2:
-    print("Usage: python script_name.py <directory_path>")
-    sys.exit(1)
+@click.command()
+@click.argument("directory_path")
+def combine_csvs(directory_path):
+    """
+    Combine multiple CSV files into one. Assumes that all CSVs have the same columns.
+    Accepts a directory path as an argument.
+    """
+    # Get all files in the directory
+    all_files = os.listdir(directory_path)
 
-DIRECTORY_PATH = sys.argv[1]
+    # Filter for CSV files
+    csv_files = [f for f in all_files if f.endswith(".csv")]
 
-# Get all files in the directory
-all_files = os.listdir(DIRECTORY_PATH)
+    # Check if any CSV files are found
+    if not csv_files:
+        click.echo("No CSV files found in the provided directory.")
+        raise click.Abort()
 
-# Filter for CSV files
-csv_files = [f for f in all_files if f.endswith(".csv")]
+    # Full paths of the CSV files
+    file_paths = [os.path.join(directory_path, file) for file in csv_files]
 
-# Check if any CSV files are found
-if not csv_files:
-    print("No CSV files found in the provided directory.")
-    sys.exit(1)
+    # Read all CSV files into DataFrames and store in a list
+    dataframes = [pd.read_csv(file_path) for file_path in file_paths]
 
-# Full paths of the CSV files
-file_paths = [os.path.join(DIRECTORY_PATH, file) for file in csv_files]
+    # Concatenate all DataFrames
+    combined_df = pd.concat(dataframes, ignore_index=True)
 
-# Read all CSV files into DataFrames and store in a list
-dataframes = [pd.read_csv(file_path) for file_path in file_paths]
+    # Remove duplicates
+    no_duplicates_df = combined_df.drop_duplicates()
 
-# Concatenate all DataFrames
-combined_df = pd.concat(dataframes, ignore_index=True)
+    # Save the result to a new CSV file in the same directory
+    output_file = os.path.join(directory_path, "combined_all_csvs.csv")
+    no_duplicates_df.to_csv(output_file, index=False)
 
-# Remove duplicates
-no_duplicates_df = combined_df.drop_duplicates()
+    click.echo(f"Combined CSV file saved to: {output_file}")
 
-# Save the result to a new CSV file in the same directory
-output_file = os.path.join(DIRECTORY_PATH, "combined_all_scis.csv")
-no_duplicates_df.to_csv(output_file, index=False)
 
-print(f"Combined CSV file saved to: {output_file}")
+if __name__ == "__main__":
+    combine_csvs()
